83e6bd2a508267901d664504aaa28922377959b8
mauryatushar281-lab <mauryatushar281@gmail.com>
2026-07-27 12:17:23 +0530

    uploads

--- src/pages/auth/GoogleSuccess/GoogleSuccess.jsx
+++ src/pages/auth/GoogleSuccess/GoogleSuccess.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { getMe } from "@/services/userApi";
+import { getMe } from "../../../services/userApi";
 
 function GoogleSuccess() {
   const navigate = useNavigate();
